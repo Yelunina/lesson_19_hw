8a6e359a3925aff2cf3fc5f7f2b59c329c638a9d
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-08-14 11:38:16 +0200

    code

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
 
+/*Используя класс SmartArray реализуйте следующую программу: пользователь с клавиатуры вводит целые числа.
+Как только пользователь вводит 0, программа выдает следующее:
+You have input the following numbers: …введенные_значения…..
+Total …колмчество_чисел… numbers
+Average value: …среднее…
+*/
 public class Main {
+    public static void main(String[] args) {
+        NumbeReader numbeReader = new NumbeReader();
+        SmartArray numbers = numbeReader.read();
+        System.out.println(numbers);
+        NumberHandler handler = new NumberHandler(numbers);
+        handler.printStat();
 
+
+    }
 }
+
+
+
+
+
+
